5d57fbd1b25ba3726853711964d47a35b26f0d6b
IMALKA-Dil <imalkadilshan0910@gmail.com>
2022-12-03 14:04:30 -0800

    50% of about is done

--- home.php
+++ home.php
@@ -5,40 +5,155 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <link rel="stylesheet" type="text/css" href="css\bootstrap-grid.min.css">
+    <link rel="stylesheet" type="text/css" href="B-css\bootstrap-grid.min.css">
 
-      <link rel="stylesheet" type="text/css" href="css\bootstrap.min.css">
+      <link rel="stylesheet" type="text/css" href="B-css\bootstrap.min.css">
        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
 
       <link rel="stylesheet" type="text/css" href="css/app.css">
     <title>Cake n' Bake</title>
 </head>
-<header>
+<divition class="home">
+
+  <header>
 
 
-  <nav>
-    <ul class="topnav" id="dropDownClick">
-      <li class="logo">Cake n' Bake</li>
-      <li><a href="#">Home</a></li>
-      <li><a href="#">About</a></li>
-      <li><a href="#">Menu</a></li>
-      <li><a href="#">Specials</a></li>
-      <li><a href="#">Events</a></li>
-      <li><a href="#">Gallery</a></li>
-      <li><a href="#">Contact</a></li>
-      <li><a href="#" class="btn-wishList">WishList</a></li>
+    <nav>
+      <ul class="topnav" id="dropDownClick">
+        <li class="logo"><span class="cake">Cake n'</span> <span class="bake">Bake</span></li>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Menu</a></li>
+        <li><a href="#">Specials</a></li>
+        <li><a href="#">Events</a></li>
+        <li><a href="#">Gallery</a></li>
+        <li><a href="#">Contact</a></li>
+        <li><a href="#" class="btn-wishList">WishList</a></li>
 
-     <li class="dropDownIcon"><a href="javascript:void(0);" onclick="dropDownMenu()">&#9776;</a></li>
-
-
-    </ul>
-  </nav>
+       <li class="dropDownIcon"><a href="javascript:void(0);" onclick="dropDownMenu()">&#9776;</a></li>
 
 
-</header>
+      </ul>
+    </nav>
 
-<body>
+
+
+  </header>
+  <div class="jumbotron jubmotron-fluid">
+
+       <div class='container'>
+        <h1 class="display-3">Cake n' Bake</h1>
+<p class="lead">Delivering great food for more than 18 years!</p>
+
+      </div>
+
+
+
+       </div>
+
+
+
+</divition>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<divition class="about">
+
+
+
+<div class="row">
+  <div class="row">
+    <div class="col">
+
+    </div>
+  </div>
+  <div class="row">
+    <div class="col">
+
+    </div>
+    <div class="col-img col-sm-12 col-md-5">
+
+    </div>
+    <div class="col">
+
+    </div>
+    <div class="col-sm-12 col-md-5">
+
+      <h1 class="abouth1">
+
+Lorem ipsum
+dolor sit amet, consectetur <br />adipiscing elit, sed do
+
+      </h1>
+
+
+<p class="aboutp1">
+  Lorem ipsum dolor sit amet, consectetur <br />
+  adipiscing elit, sed do ei<br />
+  usmod tempor incididunt ut labore et dolore magna aliqua.
+</p>
+
+<ol>
+
+  <li>
+    # Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+  </li>
+
+  <li>
+    # Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+  </li>  <li>
+      # Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+    </li>
+
+</ol>
+    </div>
+    <div class="col">
+
+    </div>
+  </div>
+  <div class="row">
+    <div class="col">
+
+    </div>
+  </div>
+</div>
+
+
+</divition>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 <script src="app.js">
 </script>
 </body>
